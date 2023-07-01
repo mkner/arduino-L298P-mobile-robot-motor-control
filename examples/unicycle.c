d849a0848e1b263fc0d8da49fe30b333c483d036
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-07-01 16:31:49 -0400

    Add files via upload

---
+++ examples/unicycle.c
@@ -0,0 +1,56 @@
+/*
+ * example using wheels function to create 
+ * basic unicycle movement
+ *  
+ * (c) 2022-2023 Mike Knerr
+ * 
+ */
+ 
+
+/*
+differential drive robot acting like a unicycle
+
+// go forward at 25% for 4 seconds
+
+unicycle(FORWARD, 25, 4000)
+
+unicycle(STOP)
+
+delay(2000)
+
+//note: wheels(STOP,2000) is same as above
+
+// spin in pos dir with rotational velocity (phi-dot) at 5% for 2 seconds
+unicycle(SPIN_LEFT, 5, 2000) 
+*/
+
+void unicycle(DIRECTION dir, int rate=0, int dur=0) {
+
+// commands are constrained to the motion of a one wheel unicycle 
+// implemented on a two wheel differential drive circular chassis robot
+
+  switch(dir) {
+
+    case FORWARD:
+      wheels(BOTH,FORWARD,rate,dur);
+    break;
+
+    case REVERSE:
+      wheels(BOTH,REVERSE,rate,dur);
+    break;
+
+    case SPIN_RIGHT:
+      wheels(BOTH,SPIN_RIGHT,rate,dur);
+    break;
+
+    case SPIN_LEFT:
+      wheels(BOTH,SPIN_LEFT,rate,dur);
+    break;
+
+    case STOP:
+      wheels(BOTH,STOP);  //,rate); //use syntax unicycle(STOP,1000); stop 1 sec or unicycle(STOP); 
+    break;
+    
+    //anything else do nothing
+  } //end switch
+}
